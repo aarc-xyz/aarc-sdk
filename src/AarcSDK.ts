295dcbb6bc680e9b975c3152d63539349687ce6e
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2023-11-01 18:30:51 +0530

    biconomy smart wallet added

--- src/AarcSDK.ts
+++ src/AarcSDK.ts
@@ -2,24 +2,23 @@ import { EthersAdapter, SafeFactory } from '@safe-global/protocol-kit';
 import { Logger } from './utils/Logger';
 import { ethers, Signer } from 'ethers';
 import { sendRequest, HttpMethod } from './utils/HttpRequest'; // Import your HTTP module
-import { BALANCES_ENDPOINT, ETHEREUM_PROVIDER, PERMIT2_CONTRACT_ADDRESS, SAFE_TX_SERVICE_URL } from './utils/Constants';
+import { BALANCES_ENDPOINT, ETHEREUM_PROVIDER, PERMIT2_CONTRACT_ADDRESS, SAFE_TX_SERVICE_URL, BICONOMY_TX_SERVICE_URL } from './utils/Constants';
 import { ExecuteMigrationDto, GetBalancesDto, TokenData } from './utils/types';
 import { OwnerResponse, BalancesResponse } from './utils/types'
 import SafeApiKit from "@safe-global/api-kit";
 import { ERC20_ABI } from './utils/abis/ERC20.abi';
 import { TokenPermissions, SignatureTransfer, PermitTransferFrom, PermitBatchTransferFrom } from '@uniswap/Permit2-sdk'
 import { ChainId } from './utils/ChainTypes';
-
+import { BiconomySmartAccountV2, DEFAULT_ENTRYPOINT_ADDRESS } from "@biconomy/account";
+import { ECDSAOwnershipValidationModule, DEFAULT_ECDSA_OWNERSHIP_MODULE } from '@biconomy/modules'
+import NodeClient from '@biconomy/node-client'
 
 class AarcSDK {
-    safeFactory!: SafeFactory;
+    chainId!: number;
     owner!: string;
-    smartWalletAddress!: string;
     saltNonce = 0;
     ethAdapter!: EthersAdapter;
-    safeService!: SafeApiKit;
-    isInited = false;
-    signer!: Signer
+    signer!: Signer;
 
     constructor(_signer: Signer) {
         Logger.log('SDK initiated');
@@ -29,16 +28,35 @@ class AarcSDK {
             ethers,
             signerOrProvider: _signer,
         });
-        this.safeService = new SafeApiKit({
-            txServiceUrl: SAFE_TX_SERVICE_URL,
-            ethAdapter: this.ethAdapter,
-        });
         this.signer = _signer
+    }
+
+    async getOwnerAddress(): Promise<string> {
+        if (this.owner == undefined) {
+            this.owner = await this.signer.getAddress();
+        }
+        return this.owner;
+    }
+
+    async getChainId(): Promise<ChainId> {
+        if (this.chainId == undefined) {
+            const chainId = await this.signer.getChainId();
+            if (chainId in Object.values(ChainId)) {
+                this.chainId = chainId;
+            } else {
+                throw new Error('Invalid chain id');
+            }
+        }
+        return this.chainId;
     }
 
     async getAllSafes(): Promise<OwnerResponse> {
         try {
-            const safes = await this.safeService?.getSafesByOwner(this.owner);
+            const safeService = new SafeApiKit({
+                txServiceUrl: SAFE_TX_SERVICE_URL,
+                ethAdapter: this.ethAdapter,
+            });     
+            const safes = await safeService.getSafesByOwner(this.owner);
             return safes;
         } catch (error) {
             Logger.log('error while getting safes');
@@ -46,35 +64,53 @@ class AarcSDK {
         }
     }
 
-    async init(): Promise<AarcSDK> {
-        try {
-            this.owner = await this.signer.getAddress()
-            // Create a SafeFactory instance using the EthersAdapter
-            this.safeFactory = await SafeFactory.create({
-                ethAdapter: this.ethAdapter,
-            });
-            const config = {
-                owners: [this.owner],
-                threshold: 1,
-            };
-            // Configure the Safe parameters and predict the Safe address
-            this.smartWalletAddress = await this.safeFactory.predictSafeAddress(
-                config,
-                this.saltNonce.toString(),
-            );
-        } catch (err) {
-            Logger.error('Error creating safe');
+    async getAllBiconomySCWs(){
+        try{
+            const nodeClient = new NodeClient({ txServiceUrl: BICONOMY_TX_SERVICE_URL });
+            const params = {
+                chainId: await this.getChainId(), //or any chain id of your choice
+                owner: await this.getOwnerAddress(),
+                index: 0
+            }
+            const accounts = await nodeClient.getSmartAccountsByOwner(params);
+            return accounts;
+        } catch (error){
+            Logger.log('error while getting biconomy smart accounts');
+            throw error;
         }
-        this.isInited = true;
-        return this
     }
 
-    /**
-     * @description this function will return computer smart wallet address
-     * @returns
-     */
-    getSafe() {
-        return this.smartWalletAddress;
+    async generateSafeSCW(): Promise<string> {
+        // Create a SafeFactory instance using the EthersAdapter
+        const safeFactory = await SafeFactory.create({
+            ethAdapter: this.ethAdapter,
+        });
+        const config = {
+            owners: [this.owner],
+            threshold: 1,
+        };
+        // Configure the Safe parameters and predict the Safe address
+        const smartWalletAddress = await safeFactory.predictSafeAddress(
+            config,
+            this.saltNonce.toString(),
+        );
+        return smartWalletAddress;
+    }
+
+    async generateBiconomySCW(): Promise<string> {
+        const module = await ECDSAOwnershipValidationModule.create({
+            signer: this.signer,
+            moduleAddress: DEFAULT_ECDSA_OWNERSHIP_MODULE
+        });
+        
+        let biconomySmartAccount = await BiconomySmartAccountV2.create({
+            chainId: await this.getChainId(),// or any supported chain of your choice 
+            entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+            defaultValidationModule: module,
+            activeValidationModule: module
+        });
+
+        return await biconomySmartAccount.getAccountAddress();
     }
 
     /**

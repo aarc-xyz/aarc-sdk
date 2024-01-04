507cd02d3d5e8b42e03713d6053857a97a998fed
talhamalik883 <talhamalik883@gmail.com>
2024-01-04 22:02:48 +0500

    final fixes

--- scripts/Constants.ts
+++ scripts/Constants.ts
@@ -9,6 +9,7 @@ export const DAPP_API_KEY = process.env.DAPP_API_KEY || ""
 
 
 export enum ChainID {
+    POLYGON_MAINNET = 137,
     GOERLI = 5,
     MUMBAI = 80001,
     ARBITRUM_GOERLI = 421613,
@@ -23,7 +24,8 @@ export enum TokenName {
     USDA1 = 'USDA1',
     USDA2 = 'USDA2',
     USDB = 'USDB',
-    USDC = 'USDC'
+    USDC = 'USDC',
+    USDT = 'USDT'
 }
 
 export const nativeTokenAddress = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
@@ -32,6 +34,7 @@ export interface TokenAddresses {
     [ChainID.GOERLI]: Record<TokenName, TokenInfo>;
     [ChainID.MUMBAI]: Record<TokenName, TokenInfo>;
     [ChainID.ARBITRUM_GOERLI]: Record<TokenName, TokenInfo>;
+    [ChainID.POLYGON_MAINNET]: Record<TokenName, TokenInfo>;
 }
 
 export const tokenAddresses: TokenAddresses = {
@@ -39,26 +42,37 @@ export const tokenAddresses: TokenAddresses = {
         [TokenName.USDA1]: { address: '0xbb8dB535d685F2742D6e84EC391c63e6a1Ce3593', decimals: 6 },
         [TokenName.USDA2]: { address: '0xf4Ca1a280ebCcdaEBf80E3C128e55DE01fAbD893', decimals: 6 },
         [TokenName.USDB]: { address: '0x2055b06Db421F17C19C655Fd4A1c325e8514aF67', decimals: 8 },
-        [TokenName.USDC]: { address: '0xd2a94832CF0a4c1A793f630264E984389C3EF48F', decimals: 18 }
+        [TokenName.USDC]: { address: '0xd2a94832CF0a4c1A793f630264E984389C3EF48F', decimals: 18 },
+        [TokenName.USDT]: { address: '', decimals: 6 }
     },
     [ChainID.MUMBAI]: {
         [TokenName.USDA1]: { address: '0xbB8bb7E16d8F03969d49fd3ED0efd13E65C8f5B5', decimals: 6 },
         [TokenName.USDA2]: { address: '0x203fa10731d98444fD59DA46705321080bA99824', decimals: 6 },
         [TokenName.USDB]: { address: '0x2D6d85C69e92F3008d9f06Ddf8Bac054783687B4', decimals: 8 },
-        [TokenName.USDC]: { address: '0xb18059aA6483bA71D6d3DfAbaD53616b00EA2ABA', decimals: 18 }
+        [TokenName.USDC]: { address: '0xb18059aA6483bA71D6d3DfAbaD53616b00EA2ABA', decimals: 18 },
+        [TokenName.USDT]: { address: '', decimals: 6 }
     },
     [ChainID.ARBITRUM_GOERLI]: {
         [TokenName.USDA1]: { address: '0x11900998de6b0C32F0bB148c6865635dfc28A528', decimals: 6 },
         [TokenName.USDA2]: { address: '0x889b795C64CdA1E9fAC1fA623E7F82A73306b690', decimals: 6 },
         [TokenName.USDB]: { address: '0x7C96ab4B90d59CE5e51f673242651018a4432672', decimals: 8 },
-        [TokenName.USDC]: { address: '0xe47a4524ad3142dE28F2F8F88b4317a439Ea89a9', decimals: 18 }
+        [TokenName.USDC]: { address: '0xe47a4524ad3142dE28F2F8F88b4317a439Ea89a9', decimals: 18 },
+        [TokenName.USDT]: { address: '', decimals: 6 }
     },
-};  
+    [ChainID.POLYGON_MAINNET]: {
+        [TokenName.USDA1]: { address: '', decimals: 6 },
+        [TokenName.USDA2]: { address: '', decimals: 6 },
+        [TokenName.USDB]: { address: '', decimals: 8 },
+        [TokenName.USDC]: { address: '0x2791bcA1f2de4661ed88a30c99a7a9449AA84174', decimals: 6 },
+        [TokenName.USDT]: { address: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', decimals: 6 }
+    }
+};
 
 export const nativeTokenAddresses = {
-    [ChainID.GOERLI] : '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+    [ChainID.GOERLI]: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
     [ChainID.MUMBAI]: '0x0000000000000000000000000000000000001010',
-    [ChainID.ARBITRUM_GOERLI] : '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+    [ChainID.POLYGON_MAINNET]: '0x0000000000000000000000000000000000001010',
+    [ChainID.ARBITRUM_GOERLI]: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
 
 }
 
